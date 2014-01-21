e7cab151492c00d5f5c85f22ef731f0e24779b77
gibiroman <gibi.roman@gmail.com>
2014-01-21 09:29:58 -0800

    added search.js

---
+++ DAW/trunk/TRAS/Scripts/app/Search/search.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    $('#searchForm').submit(function (event) {
+        event.preventDefault();
+        var item = $('#search').val();
+        $.ajax({
+            type: "POST",
+            url: '../Home/Search/',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({ query: item }),
+            success: function (data, textStatus, jqXHR) {
+
+                console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                var resp = JSON.parse(jqXHR.responseText);
+                alert(errorThrown);
+            }
+        });
+    });
+
+});
