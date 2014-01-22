de8240ce1f3058622784286f483d4890d7af09a2
Georgiana Roman <groman@eastvisionsystems.com>
2014-01-22 11:08:54 +0200

    added search.js

---
+++ TRAS/Scripts/app/Search/search.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    $('#searchForm').submit(function (event) {
+        event.preventDefault();
+        var item = $('#search').val();
+        $('#content').hide();
+        $.ajax({
+            type: "POST",
+            url: '../Home/Search/',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify({ query: item }),
+            success: function (data, textStatus, jqXHR) {
+             console.log(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                var resp = JSON.parse(jqXHR.responseText);
+                alert(errorThrown);
+            }
+
+
+        });
+
+      
+    });
+
+});
